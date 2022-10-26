7a1c75dd72c26c5051e63bdadd28ba38b7dd7d5f
likeca <like.ca@gmail.com>
2022-10-26 19:21:47 -0400

    add function

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,8 @@ class WeMo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WeMo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,37 +24,30 @@ class WeMo extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-String switchState = '0';
-String operation = 'Get'; //  $operation$option, GetBinaryState, SetBinaryState, GetSignalStrength
-String option = 'BinaryState'; // $operation: Get or Set, $option: BinaryState, BinaryState, SignalStrength
-String url = '192.168.1.102:49153';
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var devices = [];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String switchState = '0';
+  String operation = 'Get'; //  $operation$option, GetBinaryState, SetBinaryState, GetSignalStrength
+  String option = 'BinaryState'; // $operation: Get or Set, $option: BinaryState, BinaryState, SignalStrength
+
+  initWemo() {
+    for (var i = 2; i < 10; i++) {
+      devices.add({'ip': '192.168.1.10$i'});
+    }
   }
 
-  var headers = {'Accept': '*/*', 'content-type': 'text/xml; charset="utf-8"', 'SOAPACTION': '"urn:Belkin:service:basicevent:1#$operation$option"'};
-  String data = '''
+  operationWemo(ip) async {
+    String url = '$ip:49153';
+    var headers = {'Accept': '*/*', 'content-type': 'text/xml; charset="utf-8"', 'SOAPACTION': '"urn:Belkin:service:basicevent:1#$operation$option"'};
+    String data = '''
       <?xml version="1.0" encoding="utf-8"?>
       <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
           <s:Body>
@@ -72,49 +56,28 @@ class _MyHomePageState extends State<MyHomePage> {
               </u:$operation$option>
           </s:Body>
       </s:Envelope>
-  ''';
+    ''';
 
-  postData() async {
     try {
       var response = await http.post(Uri.http(url, '/upnp/control/basicevent1'), headers: headers, body: data).timeout(const Duration(seconds: 10));
+      print(response.statusCode);
       print(response.body);
     } catch (e) {
       print(e);
     }
   }
 
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -129,10 +92,10 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         // onPressed: _incrementCounter,
-        onPressed: postData,
+        onPressed: () => wemo('192.168.1.102'),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
